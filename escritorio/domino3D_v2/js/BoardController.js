d9ca0252f79c2560e6cdb32e1a99026978ce5976
Cristian Andres Camargo Giraldo <cristiancamargo95@gmail.com>
2017-11-13 11:04:15 -0500

    Dibujo inicial de un objeto 3D.

--- escritorio/domino3D_v2/js/BoardController.js
+++ escritorio/domino3D_v2/js/BoardController.js
@@ -4,11 +4,54 @@ DOMINO.BoardController = function (options) {
     options = options || {};
 
     var containerEl = options.containerEl || null;
-
     var assetsUrl = options.assetsUrl || '';
 
+	var renderer;
+	var scene;
+	var camera;
+	var cameraController;
+
     this.drawBoard = function () {
-        console.log('drawBoard');
-    };
+    	initEngine();
+    	initObjects(function () {
+		    onAnimationFrame();
+		});
+	};
+
+	function initEngine() {
+	    var viewWidth = containerEl.offsetWidth;
+	    var viewHeight = containerEl.offsetHeight;
+
+	    // instantiate the WebGL Renderer
+	    renderer = new THREE.WebGLRenderer({
+	        antialias: true
+	    });
+	    renderer.setSize(viewWidth, viewHeight);
+
+	    // create the scene
+	    scene = new THREE.Scene();
+
+	    // create camera
+	    camera = new THREE.PerspectiveCamera(35, viewWidth / viewHeight, 1, 1000);
+	    camera.position.set(0, 120, 150);
+	    cameraController = new THREE.OrbitControls(camera, containerEl);
+	    //
+	    scene.add(camera);
+
+	    containerEl.appendChild(renderer.domElement);
+	}
+
+	function initObjects(callback) {
+	    var cube = new THREE.Mesh(new THREE.CubeGeometry(50, 50, 50));
+	    scene.add(cube);
+
+	    callback();
+	}
+
+	function onAnimationFrame() {
+	    requestAnimationFrame(onAnimationFrame);
+	    cameraController.update();
+	    renderer.render(scene, camera);
+	}
 };
 
